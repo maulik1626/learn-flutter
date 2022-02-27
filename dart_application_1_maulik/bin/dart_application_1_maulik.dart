ce758c9a9820124ed8f4d0c6c028cb6f44118841
maulik <rajamaulik9@gmail.com>
2022-02-28 01:30:24 +0530

    maulik

--- dart_application_1_maulik/bin/dart_application_1_maulik.dart
+++ dart_application_1_maulik/bin/dart_application_1_maulik.dart
@@ -1,3 +1,150 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  //   var a = 123;
+//   var f_name = "Maulik";
+//   var myAddress = "Amd";
+//   String name = "Maulik ";
+//   int number = 123;
+//   double float = 12.12;
+//   print(a);
+//   print(f_name);
+//   print(myAddress);
+//   print(name);
+//   print(number);
+//   print(float);
+
+// String many = """ Hello
+// me  """;
+
+// print(many.runtimeType);
+
+// bool isVerified = false;
+
+// print(isVerified.runtimeType);
+
+// print(isVerified);
+
+  // var a = 10;
+  // var b = 7;
+
+  // print(a + b); // Add
+  // print(a - b); // Sub
+  // print(a * b); // Mul
+  // print(a / b); // Div
+  // print(a ~/ b); // Div with round number with floor value
+  // print(a % b); // Modulo
+
+  // print("${a} + ${b} = ${a+b}");
+
+  // print("Hello, \nI am Maulik");
+
+  // var i = 10;
+
+  // print(i);
+  // i++;
+  // print(i);
+  // ++i;
+  // print(i);
+
+  // i--;
+  // print(i);
+  // --i;
+  // print(i);
+
+  // var a = 100;
+  // var b = 200;
+
+  // print(a < b);
+  // print(a > b);
+  // print(a <= b);
+  // print(a >= b);
+  // print(a != b);
+  // print(a == b);
+
+  /*
+  Logical Operators
+  and = &&
+  or = ||
+  */
+
+  // var a = 10;
+  // var b = 10;
+  // var c = 20;
+  // var d = 20;
+
+  // print((a == b) && (c == d));
+  // print((a == b) && (c != d));
+  // print((a == b) || (c != d));
+  // print((a == b) || (c == d));
+
+  /*
+  Type test operator
+  is 
+  is! == is not
+  */
+
+  // var name = "Maulik";
+  // var age = 26;
+
+  // print(name is String);
+  // print(age is int);
+  // print(age is! int);
+
+  // Ternary operator OR conditional expression
+  /* 
+    Syntax:
+
+    condition ? true : false;
+    condition ?? experssion;      // if the value is null on the left hand side of ??, the value of right hand side will be condsidered
+
+  */
+
+  // var a = 100;
+  // var b = 200;
+
+  // a > b ? b += 50 : a += 50;
+
+  // print(a);
+  // print(b);
+
+  // var num1 = 100;
+  // var num2 = 400;
+  // var num3 = 300;
+
+  // num1 > num2 && num1 > num3 ? print("num1 is bigger") : num2 > num3 ? print("num2 is bigger") : print("num3 is bigger");
+
+  ////var name = "Maulik";
+  // var name = null;
+
+  // var user = name ?? "Guest";
+
+  // print(user);
+
+  // String name1 = "Maulik";
+
+  // print(name1);
+  // print(name1.runtimeType);
+
+  // var a = 'Welcome to Maulik\'s Dart class';
+
+  // print(a);
+
+  // var b = 20;
+
+  // print(a + " " + b.toString());
+
+  // To convert the dataType or make typeCaste, use .to... method
+
+  String name = "Maulik";
+  String age = "26";
+
+  print("Hello, $name");
+  print("Hello, ${name + ' ' + age}");
+
+  print("${name.toUpperCase()}");
+  print("${name.toLowerCase()}");
+  
+  String? name1 = stdin.readLineSync();
+  print("Hello, $name1");
 }
