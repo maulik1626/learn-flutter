9e4c8e5bb271e9ea8c55c36715c44f910549e9f7
maulik <rajamaulik9@gmail.com>
2022-03-04 22:30:58 +0530

    Maulik commit

--- dart_application_1_maulik/bin/assignments.dart
+++ dart_application_1_maulik/bin/assignments.dart
@@ -2,6 +2,7 @@ import 'calculator.dart';
 import 'sqr_cube.dart';
 import 'circle_area.dart';
 import 'triangle_area.dart';
+
 void main(List<String> args) {
   // q_1();
   // q_2();
@@ -10,37 +11,30 @@ void main(List<String> args) {
   q_5();
 }
 
-void q_1(){
+void q_1() {
   print("Maulik");
   print("16/03/1995");
   print(26);
   print("Shripad residency");
 }
 
-void q_2(){
-
+void q_2() {
   calc();
-
 }
 
-void q_3(){
-
-  var lst =   sqr_cube_maker();
+void q_3() {
+  var lst = sqr_cube_maker();
 
   print("The square is : ${lst[0]}");
   print("The cube is ${lst[1]}");
-
 }
 
-void q_4(){
-
+void q_4() {
   var area = circle();
   print("The area of the circle is $area");
-
 }
 
 void q_5() {
-
   var area = triangle();
 
   print("The area of the triangle is $area");
